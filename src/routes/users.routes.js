60f81da76cf4af3e5440067d0fc7841e2b7ebaca
diego <dmarques1508@gmail.com>
2024-01-03 16:22:47 -0300

    saving images

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,15 +7,14 @@ const usersRoutes = Router()
 const upload = multer(uploading.MULTER)
 
 const UsersController = require("../controller/UsersController")
+const UserAvatarController = require("../controller/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
-usersRoutes.patch("/avatar",ensureAuthenticated, upload.single("avatar"), (request, response) => {
-    console.log(request.file.filename)
-    response.json()
-})
+usersRoutes.patch("/avatar",ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
